7689f0696503a48e448c1f1fa4801be1273eda56
tigercxx <76241201+tigercxx@users.noreply.github.com>
2022-12-11 04:08:58 +0800

    Added comments

--- storeBlob.js
+++ storeBlob.js
@@ -18,33 +18,21 @@ app.use(function (req, res, next) {
     next();
 });
 
-// app.get("/", (req, res) => {
-//     console.log("hi");
-//     res.json({ message: "Hello World!" });
-// });
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
-
+// token from NFTStorage
+// Add your API_KEY in a .env file
 const token = process.env.API_KEY;
 const storage = new NFTStorage({ token });
 const upload = multer({ dest: "uploads/" });
 app.post("/upload_article", upload.single("file"), async (req, res) => {
-    // console.log("NEW POST");
-    // console.log(req.body);
+    // expects a file: FileObject in body of request
     const file = req.file;
-    // console.log(file);
     const filebuffer = fs.readFileSync(file.path);
-    // console.log(filebuffer);
     const blob = new Blob([filebuffer]);
-    // console.log(blob);
-    // const fileObject = new File([blob], file.originalname, {
-    //     type: "application/pdf",
-    // });
-    // console.log(fileObject);
-
     try {
+        // returns a cid of the file stored on NFTStorage
         const cid = await storage.storeBlob(blob);
         res.json({
             link: `https://nftstorage.link/ipfs/${cid}`,
@@ -53,6 +41,4 @@ app.post("/upload_article", upload.single("file"), async (req, res) => {
         console.error(err);
         console.log(err.message);
     }
-    // const cid = metadata.data.host;
-    // res.json({ link: `https://nftstorage.link/ipfs/${cid}` });
 });
